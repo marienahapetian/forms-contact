8b8465a447d6e2042908864396cb6a1a429ad944
Harut <ginosyan.harut@gmail.com>
2017-05-05 14:01:51 +0400

    Updated links

--- admin/hugeit_free_version.php
+++ admin/hugeit_free_version.php
@@ -14,7 +14,7 @@ function hugeit_contact_drawFreeBanner( $freeText = 'no' ) {
 		<a class="close_free_banner">+</a>
 		<img class="manual_icon" src="<?php echo $path_site2; ?>/icon-user-manual.png" alt="user manual" />
 		<p class="usermanual_text">If you have any difficulties in using the options, Follow the link to <a href="http://huge-it.com/wordpress-forms-user-manual/" target="_blank">User Manual</a></p>
-		<a class="get_full_version" href="http://huge-it.com/forms/" target="_blank">GET THE FULL VERSION</a>
+		<a class="get_full_version" href="https://goo.gl/ycVtso" target="_blank">GET THE FULL VERSION</a>
 		<a href="http://huge-it.com" target="_blank"><img class="huge_it_logo" src="<?php echo $path_site2; ?>/Huge-It-logo.png"/></a>
 		<div style="clear: both;"></div>
 		<div class="hg_social_link_buttons">
@@ -25,8 +25,8 @@ function hugeit_contact_drawFreeBanner( $freeText = 'no' ) {
 		</div>
 		<div class="hg_view_plugins_block">
 			<a target="_blank"  href="https://wordpress.org/support/plugin/forms-contact/reviews">Rate Us</a>
-			<a target="_blank"  href="http://huge-it.com/wordpress-forms-demo-1-delivery-form/">Full Demo</a>
-			<a target="_blank"  href="http://huge-it.com/wordpress-forms-faq/">FAQ</a>
+			<a target="_blank"  href="https://goo.gl/XWGBxi">Full Demo</a>
+			<a target="_blank"  href="https://goo.gl/rpDoYY">FAQ</a>
 			<a target="_blank"  href="http://huge-it.com/contact-us/">Contact Us</a>
 		</div>
 		<div  class="description_text"><p>This is the LITE version of the plugin. Click "GET THE FULL VERSION" for more advanced options. We appreciate every customer.</p></div>
