503c0d7f6eeaf14125ce226adc63d87dd094ef01
marienahapetian <marie.nahapetian@gmail.com>
2017-11-28 14:20:45 +0400

    added render method to the class

--- includes/class-hugeit-contact-template-loader.php
+++ includes/class-hugeit-contact-template-loader.php
@@ -78,4 +78,16 @@ class Hugeit_Contact_Template_Loader {
 		do_action( 'hugeit_contact_after_template_part', $template_name, $template_path, $located, $args );
 	}
 
+
+    public static function render($html_path, $params = array(), $css_path='') {
+        ob_start();
+        ob_implicit_flush(false);
+        extract($params, EXTR_OVERWRITE);
+        require $html_path;
+        if ( $css_path ) {
+            require $css_path;
+        }
+        return ob_get_clean();
+    }
+
 }
